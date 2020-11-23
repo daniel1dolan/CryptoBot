fca51d10df63a1a07de4ddab3bf0f5e57ec6e6d1
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-11-22 21:22:59 -0600

    added proper macd strategy with 200 ema

--- src/strategies/simpleMACD.js
+++ src/strategies/simpleMACD.js
@@ -8,42 +8,55 @@ class SimpleMACD extends Strategy {
     const shortPeriod = 12;
     const longPeriod = 26;
     const signalPeriod = 9;
+    const emaPeriod = 200;
     const indicator = tulind.indicators.macd.indicator;
+    const emaIndicator = tulind.indicators.ema.indicator;
 
     const results = await indicator(
       [prices],
       [shortPeriod, longPeriod, signalPeriod]
     );
+    const emaRes = await emaIndicator([prices], [emaPeriod]);
 
     const histogram = results[2];
     const signal = results[1];
     const macd = results[0];
+    const ema = emaRes[0];
 
-    const length = histogram.length;
+    const length = signal.length;
+    const emaLength = ema.length;
     if (length < 2) {
       return;
     }
-    const penultimate = histogram[length - 2];
-    const last = histogram[length - 1];
+    const penultimateSignal = signal[length - 2];
+    const lastSignal = signal[length - 1];
+    const penultimateMACD = macd[length - 2];
+    const lastMACD = macd[length - 1];
+    const lastEMA = ema[emaLength - 1];
 
     const boundary = 0;
 
-    const wasAbove = penultimate > boundary;
-    const wasBelow = penultimate < -boundary;
-    const isAbove = last > boundary;
-    const isBelow = last < -boundary;
+    const signalBelowZero = lastSignal < boundary;
+    const wasAbove = penultimateMACD > penultimateSignal;
+    const wasBelow = penultimateMACD < penultimateSignal;
+    const isAbove = lastMACD > lastSignal;
+    const isBelow = lastMACD < lastSignal;
 
     const open = this.openPositions();
 
     const price = sticks[sticks.length - 1].close;
 
-    if (open.length == 0) {
-      if (wasAbove && isBelow) {
+    const priceAboveEMA = price > lastEMA;
+
+    if (open.length === 0) {
+      if (signalBelowZero && priceAboveEMA && wasBelow && isAbove) {
         this.onBuySignal({ price, time });
       }
     } else {
       open.forEach((p) => {
-        if (isAbove && wasBelow) {
+        if (p.enter.price * 1.02 <= price) {
+          this.onSellSignal({ price, time, size: p.enter.size, position: p });
+        } else if (wasAbove && isBelow) {
           if (p.enter.price * 1.02 <= price) {
             this.onSellSignal({ price, time, size: p.enter.size, position: p });
           }
